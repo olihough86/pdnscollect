cab807acf585bfc4ea16863dee09ebd1a8af9e56
Oliver Hough <4314648+olihough86@users.noreply.github.com>
2023-04-14 12:29:56 +0200

    implemented elasticsearch 8.x

--- api/main.go
+++ api/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/gorilla/mux"
 )
 
@@ -14,15 +18,55 @@ type DNSData struct {
 	Domain    string `json:"domain"`
 }
 
+var esClient *elasticsearch.Client
+
+func init() {
+	var err error
+	esClient, err = elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error initializing Elasticsearch client: %s", err)
+	}
+}
+
 var dnsDataList []DNSData
 
 func submitDNSData(w http.ResponseWriter, r *http.Request) {
 	var newDNSData DNSData
 	json.NewDecoder(r.Body).Decode(&newDNSData)
-	dnsDataList = append(dnsDataList, newDNSData)
 
 	// Log received DNS data to the terminal
 	log.Printf("Received DNS data: ID=%s, IP=%s, Domain=%s", newDNSData.ID, newDNSData.IPAddress, newDNSData.Domain)
+
+	// Convert the DNS data struct to JSON
+	jsonData, err := json.Marshal(newDNSData)
+	if err != nil {
+		log.Printf("Error converting DNS data to JSON: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Index the DNS data in Elasticsearch
+	req := esapi.IndexRequest{
+		Index:      "dnsdata",
+		DocumentID: newDNSData.ID,
+		Body:       strings.NewReader(string(jsonData)),
+		Refresh:    "true",
+	}
+
+	res, err := req.Do(r.Context(), esClient)
+	if err != nil {
+		log.Printf("Error sending DNS data to Elasticsearch: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		errMsg := fmt.Sprintf("Elasticsearch error: %s", res.String())
+		log.Printf(errMsg)
+		http.Error(w, errMsg, http.StatusInternalServerError)
+		return
+	}
 
 	json.NewEncoder(w).Encode(newDNSData)
 }
